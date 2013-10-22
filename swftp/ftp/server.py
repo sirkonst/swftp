970a3166e3f457c327d0f66163fb92c6b7656689
Konstantin Enchant <sirkonst@gmail.com>
2013-10-22 13:10:13 +0400

    Fixed DTP socket leaks.

--- swftp/ftp/server.py
+++ swftp/ftp/server.py
@@ -88,23 +88,15 @@ class SwftpFTPProtocol(FTP, object):
         Overwrite cleanupDTP() for fix socket leak
         (see http://twistedmatrix.com/trac/ticket/5367)
         """
-        log.msg('cleanupDTP', debug=True)
-
-        log.msg(self.dtpPort)
-        dtpPort, self.dtpPort = self.dtpPort, None
-        if interfaces.IListeningPort.providedBy(dtpPort):
-            dtpPort.stopListening()
-        elif interfaces.IConnector.providedBy(dtpPort):
-            dtpPort.disconnect()
-        else:
-            assert False, "dtpPort should be an IListeningPort or IConnector, instead is %r" % (dtpPort,)
-
-        self.dtpFactory.stopFactory()
-        self.dtpFactory = None
-
+        transport = None
         if self.dtpInstance is not None:
-            self.dtpInstance.transport.abortConnection()
-            self.dtpInstance = None
+            if self.dtpInstance.transport is not None:
+                transport = self.dtpInstance.transport
+
+        super(SwftpFTPProtocol, self).cleanupDTP()
+
+        if transport:
+            transport.abortConnection()
 
 
 class SwiftFTPShell:
